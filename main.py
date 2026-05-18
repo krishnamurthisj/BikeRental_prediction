682c429260463b1c32ba7cb7bf46abfc39cba0f2
Krishna Murthi sj <krishnamurthisj3@gmail.com>
2026-05-18 15:31:59 +0530

    Update prediction URL for bike demand model

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ with tab1:
             st.dataframe(input_df)
 
         if st.button("🔮 Predict Bike Demand"):
-            url = "http://127.0.0.1:8000/docs"
+            url = "https://bikerental-prediction.onrender.com"
             try:
                 response = requests.post(url, json=data)
                 if response.status_code == 200:
